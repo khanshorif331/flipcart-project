3d3c2414cf2f3ed92a09f5dcb597840884e60b52
khanshorif331 <khanshorif331@gmail.com>
2022-07-23 00:02:52 +0600

    header section added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg'
 import './App.css'
+import Header from './components/Header/Header'
+
+// components
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>Hell world</h1>
+			<Header></Header>
 		</div>
 	)
 }
