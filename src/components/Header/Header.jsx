e2e0c461be34df61234b1969bbcb9e76c0c5f353
khanshorif331 <khanshorif331@gmail.com>
2022-07-23 00:31:56 +0600

    header coloor and height changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,8 @@ import { AppBar,Toolbar , styled} from '@mui/material';
 import React from 'react';
 
 const StyledHeader = styled(AppBar)`
-  background-color: #2874f0
-`
+  background-color: #2874f0;
+  height: 55px;`
 
 const Header = () => {
     return (
