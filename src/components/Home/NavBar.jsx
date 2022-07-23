d8e17c381fe85fe326aec18b26bc794544ff0956
khanshorif331 <khanshorif331@gmail.com>
2022-07-23 18:53:51 +0600

    data loaded on the ui

--- src/components/Home/NavBar.jsx
+++ src/components/Home/NavBar.jsx
@@ -1,19 +1,27 @@
-import { Box } from '@mui/material';
+import { Box, styled } from '@mui/material';
 import React from 'react';
 import { navData } from '../../constants/data';
 
+const Component = styled(Box)`
+    display: flex;
+`
+
 const NavBar = () => {
     return (
-        <Box>
+        <Component>
             {
-                navData.map(data =>{
-                    return <Box>
+                navData.map(data => {
+                   return (
+                   <Box>
                         <img src={data.url} alt='nav'></img>
                         <p>{data.text}</p>
                     </Box>
-                })
+                    )
+                   }
+                    
+                )
             }
-        </Box>
+        </Component>
     );
 };
 
