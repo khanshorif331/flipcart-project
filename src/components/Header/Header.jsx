96a28666ce036e160e42f069c9e25d7d394410ea
khanshorif331 <khanshorif331@gmail.com>
2022-07-23 11:59:26 +0600

    logo updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
-import { AppBar,Toolbar , styled, Box} from '@mui/material';
+import { AppBar,Toolbar , styled, Box, Typography} from '@mui/material';
 import React from 'react';
 
 const StyledHeader = styled(AppBar)`
   background-color: #2874f0;
   height: 55px;`
+
+const Component = styled(Box)`
+  margin-left: 12%;
+  line-height: 0;
+`
+const SubHeading = styled(Typography)`
+  font-size: 10px;
+  font-style: italic;
+`
 
 const Header = () => {
   const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
@@ -11,9 +20,12 @@ const Header = () => {
         <div>
            <StyledHeader>
             <Toolbar>
-              <Box>
+              <Component>
                 <img src={logoURL} alt="logo" style={{width:75}}/>
-              </Box>
+                <Box>
+                  <SubHeading>Explore Plus</SubHeading>
+                </Box>
+              </Component>
             </Toolbar>
            </StyledHeader>
         </div>
