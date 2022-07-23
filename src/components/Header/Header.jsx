a8ec9340e637a24a0b30bca2b0da1d5a656e46fe
khanshorif331 <khanshorif331@gmail.com>
2022-07-23 12:18:33 +0600

    lonoo finalize done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,21 +14,32 @@ const SubHeading = styled(Typography)`
   font-style: italic;
 `
 
+// handling style for an html element
+const PlusImage = styled('img')({
+    width: 10,
+    height: 10,
+    marginLeft: 4,
+})
+
+
 const Header = () => {
   const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
+  const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
+
     return (
-        <div>
            <StyledHeader>
             <Toolbar>
               <Component>
                 <img src={logoURL} alt="logo" style={{width:75}}/>
-                <Box>
-                  <SubHeading>Explore Plus</SubHeading>
+                <Box style={{display:'flex'}}>
+                  <SubHeading>Explore&nbsp; 
+                    <Box component='span' style={{color:'#FFE500'}}>Plus</Box>
+                  </SubHeading>
+                  <PlusImage src={subURL} alt="sub-logo" />
                 </Box>
               </Component>
             </Toolbar>
            </StyledHeader>
-        </div>
     );
 };
 
