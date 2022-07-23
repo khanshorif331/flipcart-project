858e6df2b7d67f06b8aa53cb7da10778f5044676
khanshorif331 <khanshorif331@gmail.com>
2022-07-23 18:05:28 +0600

    data.js file added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg'
-import './App.css'
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import { Box } from '@mui/material'
--- src/components/Home/NavBar.jsx
+++ src/components/Home/NavBar.jsx
@@ -1,10 +1,19 @@
+import { Box } from '@mui/material';
 import React from 'react';
+import { navData } from '../../constants/data';
 
 const NavBar = () => {
     return (
-        <div>
-            <p>Hello from navbar</p>
-        </div>
+        <Box>
+            {
+                navData.map(data =>{
+                    return <Box>
+                        <img src={data.url} alt='nav'></img>
+                        <p>{data.text}</p>
+                    </Box>
+                })
+            }
+        </Box>
     );
 };
 
