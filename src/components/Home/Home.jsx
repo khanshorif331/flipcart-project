57e7628e3c3a93fa230a5770f8a113bbf92aa80a
khanshorif331 <khanshorif331@gmail.com>
2022-07-23 21:14:08 +0600

    banner carousel perfectly donee

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,22 @@
+import { Box, styled } from '@mui/material';
 import React from 'react';
 import Banner from './Banner';
 import NavBar from './NavBar';
+
+
+const Component = styled (Box)`
+    padding: 10px;
+    background-color: #F2F2F2;
+`
+
 
 const Home = () => {
     return (
         <>
             <NavBar></NavBar>
-            <Banner></Banner>
+            <Component>
+                <Banner></Banner>
+            </Component>
         </>
     );
 };
