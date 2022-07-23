4636378fc26e8311ed7449d285a44487480a4a21
khanshorif331 <khanshorif331@gmail.com>
2022-07-24 02:14:06 +0600

    added lgin ppup succesfully

--- src/components/Login/LoginDialogue.js
+++ src/components/Login/LoginDialogue.js
@@ -1,5 +1,24 @@
-import { Box, Dialog, TextField, Button, Typography } from '@mui/material'
+import {
+	Box,
+	Dialog,
+	TextField,
+	Button,
+	Typography,
+	styled,
+} from '@mui/material'
 import React from 'react'
+
+const Component = styled(Box)`
+	height: 70vh;
+	width: 90vw;
+`
+//  https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png
+
+const Image = styled(Box)`
+	background: #2874f0;
+	height: 100%;
+	width: 20%;
+`
 
 const LoginDialogue = ({ open, setOpen }) => {
 	const handleClose = () => {
@@ -7,24 +26,29 @@ const LoginDialogue = ({ open, setOpen }) => {
 	}
 	return (
 		<Dialog open={open} onClose={handleClose}>
-			<Box>
-				<Box></Box>
-				<Box>
-					<TextField
-						variant='standard'
-						label='Enter Email/Mobile Number'
-					></TextField>
-					<TextField variant='standard' label='Enter Password'></TextField>
-					<TextField>
-						By continuing, you agree to Flipkart's Terms of Use and
-						Privacy Policy.
-					</TextField>
-					<Button>Login</Button>
-					<Typography>OR</Typography>
-					<Button>Login</Button>
-					<Typography>New to Flipkart? Create an account</Typography>
+			<Component>
+				<Box style={{ display: 'flex', height: '100%' }}>
+					<Image></Image>
+					<Box>
+						<TextField
+							variant='standard'
+							label='Enter Email/Mobile Number'
+						></TextField>
+						<TextField
+							variant='standard'
+							label='Enter Password'
+						></TextField>
+						<TextField>
+							By continuing, you agree to Flipkart's Terms of Use and
+							Privacy Policy.
+						</TextField>
+						<Button>Login</Button>
+						<Typography>OR</Typography>
+						<Button>Login</Button>
+						<Typography>New to Flipkart? Create an account</Typography>
+					</Box>
 				</Box>
-			</Box>
+			</Component>
 		</Dialog>
 	)
 }
