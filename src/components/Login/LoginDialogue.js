317e21c7033b0e131bef49f053162cb5ebd55e9d
khanshorif331 <khanshorif331@gmail.com>
2022-07-24 21:33:29 +0600

    handling the style of the login dialoog

--- src/components/Login/LoginDialogue.js
+++ src/components/Login/LoginDialogue.js
@@ -10,16 +10,34 @@ import React from 'react'
 
 const Component = styled(Box)`
 	height: 70vh;
-	width: 90vw;
+	width: 50vw;
 `
 //  https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png
 
 const Image = styled(Box)`
 	background: #2874f0
 		url(https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png)
-		no-repeat center;
+		no-repeat center 85%;
 	height: 100%;
-	width: 20%;
+	width: 30%;
+	padding: 45px 35px;
+`
+
+const Wrapper = styled(Box)`
+	display: flex;
+	flex-direction: column;
+	padding: 25px 35px;
+	flex: 1;
+	& > div,
+	& > button,
+	& > p {
+		margin-top: 20px;
+	}
+`
+
+const LoginButton = styled(Button)`
+	text-transform: none;
+	background: #fb641b;
 `
 
 const LoginDialogue = ({ open, setOpen }) => {
@@ -30,8 +48,13 @@ const LoginDialogue = ({ open, setOpen }) => {
 		<Dialog open={open} onClose={handleClose}>
 			<Component>
 				<Box style={{ display: 'flex', height: '100%' }}>
-					<Image></Image>
-					<Box>
+					<Image>
+						<Typography variant='h5'>Login</Typography>
+						<Typography style={{ marginTop: 20 }}>
+							Get access to your Orders, Wishlist and Recommendations
+						</Typography>
+					</Image>
+					<Wrapper>
 						<TextField
 							variant='standard'
 							label='Enter Email/Mobile Number'
@@ -40,15 +63,15 @@ const LoginDialogue = ({ open, setOpen }) => {
 							variant='standard'
 							label='Enter Password'
 						></TextField>
-						<TextField>
+						<Typography>
 							By continuing, you agree to Flipkart's Terms of Use and
 							Privacy Policy.
-						</TextField>
-						<Button>Login</Button>
+						</Typography>
+						<LoginButton>Login</LoginButton>
 						<Typography>OR</Typography>
-						<Button>Login</Button>
+						<Button>Request OTP</Button>
 						<Typography>New to Flipkart? Create an account</Typography>
-					</Box>
+					</Wrapper>
 				</Box>
 			</Component>
 		</Dialog>
