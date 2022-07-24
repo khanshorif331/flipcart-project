9cd890985b6082615ca9a91623fbeaa6ad4beb36
khanshorif331 <khanshorif331@gmail.com>
2022-07-24 23:44:12 +0600

    signup page changed

--- src/components/Login/LoginDialogue.js
+++ src/components/Login/LoginDialogue.js
@@ -6,13 +6,12 @@ import {
 	Typography,
 	styled,
 } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Component = styled(Box)`
 	height: 70vh;
 	width: 50vw;
 `
-//  https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png
 
 const Image = styled(Box)`
 	background: #2874f0
@@ -21,6 +20,11 @@ const Image = styled(Box)`
 	height: 100%;
 	width: 30%;
 	padding: 45px 35px;
+	& > p,
+	& > h5 {
+		color: #fff;
+		font-weight: 600;
+	}
 `
 
 const Wrapper = styled(Box)`
@@ -38,14 +42,48 @@ const Wrapper = styled(Box)`
 const LoginButton = styled(Button)`
 	text-transform: none;
 	background: #fb641b;
+	color: #fff;
+	height: 48px;
+	border-radius: 2px;
+`
+const RequestOTP = styled(Button)`
+	text-transform: none;
+	background: #fff;
+	color: #287480;
+	height: 48px;
+	border-radius: 2px;
+	box-shadow: 0px 2px 4px 0 rgba(0 0 0/ 20%);
+`
+
+const Text = styled(Typography)`
+	font-size: 12px;
+	color: #878787;
+`
+
+const CreateAccount = styled(Typography)`
+	font-size: 14px;
+	text-align: center;
+	color: #2874f0;
+	font-weight: 600;
+	cursor: pointer;
 `
 
 const LoginDialogue = ({ open, setOpen }) => {
+	const [signup, setSignup] = useState(true)
+
+	const handleSignup = () => {
+		setSignup(false)
+	}
+
 	const handleClose = () => {
 		setOpen(false)
 	}
 	return (
-		<Dialog open={open} onClose={handleClose}>
+		<Dialog
+			open={open}
+			onClose={handleClose}
+			PaperProps={{ sx: { maxWidth: 'unset' } }}
+		>
 			<Component>
 				<Box style={{ display: 'flex', height: '100%' }}>
 					<Image>
@@ -54,24 +92,48 @@ const LoginDialogue = ({ open, setOpen }) => {
 							Get access to your Orders, Wishlist and Recommendations
 						</Typography>
 					</Image>
-					<Wrapper>
-						<TextField
-							variant='standard'
-							label='Enter Email/Mobile Number'
-						></TextField>
-						<TextField
-							variant='standard'
-							label='Enter Password'
-						></TextField>
-						<Typography>
-							By continuing, you agree to Flipkart's Terms of Use and
-							Privacy Policy.
-						</Typography>
-						<LoginButton>Login</LoginButton>
-						<Typography>OR</Typography>
-						<Button>Request OTP</Button>
-						<Typography>New to Flipkart? Create an account</Typography>
-					</Wrapper>
+					{signup ? (
+						<Wrapper>
+							<TextField
+								variant='standard'
+								label='Enter Email/Mobile Number'
+							></TextField>
+							<TextField
+								variant='standard'
+								label='Enter Password'
+							></TextField>
+							<Text>
+								By continuing, you agree to Flipkart's Terms of Use and
+								Privacy Policy.
+							</Text>
+							<LoginButton>Login</LoginButton>
+							<Typography style={{ textAlign: 'center' }}>OR</Typography>
+							<RequestOTP>Request OTP</RequestOTP>
+							<CreateAccount onClick={handleSignup}>
+								New to Flipkart? Create an account
+							</CreateAccount>
+						</Wrapper>
+					) : (
+						<Wrapper>
+							<TextField
+								variant='standard'
+								label='Enter Email/Mobile Number'
+							></TextField>
+							<TextField
+								variant='standard'
+								label='Enter Password'
+							></TextField>
+
+							<RequestOTP>Continue</RequestOTP>
+							<CreateAccount
+								onClick={() => {
+									setSignup(true)
+								}}
+							>
+								Existing User? Login
+							</CreateAccount>
+						</Wrapper>
+					)}
 				</Box>
 			</Component>
 		</Dialog>
