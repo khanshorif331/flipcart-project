8d08831c91637d583c873ec7ef7d7b6d22c0119c
khanshorif331 <khanshorif331@gmail.com>
2022-08-05 23:53:47 +0600

    context api created successfully

--- src/context/DataProvider.jsx
+++ src/context/DataProvider.jsx
@@ -1,4 +1,20 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 
-const DataContext = createContext(null);
+const DataContext = createContext(null)
+
+
+const DataProvider = ( ) => {
+    const [account,setAccount] = useState('')
+
+    return (
+        <DataContext.Provider value={{
+            account,
+            setAccount
+        }}>
+
+        </DataContext.Provider>
+    )
+}
+
+export default DataProvider
