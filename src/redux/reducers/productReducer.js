abab3d59a09773645fc1da3292357b02c3b065a7
khanshorif331 <khanshorif331@gmail.com>
2022-08-06 23:54:02 +0600

    product reducer creating done almoost

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,1 +1,13 @@
-export const getProductsReducer = () => {}
+// import * as actionType from '../constants/productConstants.js'
+import * as actionType from '../constants/productConstant'
+
+export const getProductsReducer = (state = { products: [] }, action) => {
+	switch (action.type) {
+		case actionType.GET_PRODUCTS_SUCCESS:
+			return { products: action.payload }
+		case actionType.GET_PRODUCTS_FAILED:
+			return { error: action.payload }
+		default:
+			return state
+	}
+}
