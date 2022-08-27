13f1ce8ba330197fc3c3f4aa80bfbb4cf5458c75
khanshorif331 <khanshorif331@gmail.com>
2022-08-27 20:50:04 +0600

    icon added in teh offers

--- src/components/Details/ProductDetail.jsx
+++ src/components/Details/ProductDetail.jsx
@@ -1,23 +1,35 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
+import {LocalOffer as Badge} from '@mui/icons-material';
 
 const ProductDetail = ({product}) => {
     const fassured = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png'
     return (
-        <div>
+        <>
             <Typography>{product.title.longTitle}</Typography>
-                        <Typography style={{marginTop : 5,color:'#878787',fontSize:14}}>
+            <Typography style={{marginTop : 5,color:'#878787',fontSize:14}}>
                             8 Ratings & 1 Reviews
-                            <Box component='span'>
-                                <img src={fassured} style={{width:77,marginLeft:20}} alt="" />
-                            </Box>
-                        </Typography>
-                        <Typography>
-                            <Box component='span' style={{fontSize:28}}>₹{product.price.cost}</Box>&nbsp;&nbsp;&nbsp; 
-                            <Box component='span' style={{color:'#878787'}}><strike>{product.price.mrp}</strike></Box>&nbsp;&nbsp;&nbsp;
-                            <Box component='span' style={{color:'#388E3C'}}>{product.price.discount}</Box>
-                        </Typography>
-        </div>
+                <Box component='span'>
+                    <img src={fassured} style={{width:77,marginLeft:20}} alt="" />
+                </Box>
+            </Typography>
+            <Typography>
+                <Box component='span' style={{fontSize:28}}>₹{product.price.cost}</Box>&nbsp;&nbsp;&nbsp; 
+                <Box component='span' style={{color:'#878787'}}><strike>{product.price.mrp}</strike></Box>&nbsp;&nbsp;&nbsp;
+                <Box component='span' style={{color:'#388E3C'}}>{product.price.discount}</Box>
+            </Typography>
+            <Typography>
+                   Available offers :
+            </Typography>          
+            <Box>
+                <Typography><Badge/>10% instant discount on SBI Mastercard Debit Cards, up to ₹250 on orders of ₹2,500 and above T&C</Typography>
+                <Typography><Badge/>5% Cashback on Flipkart Axis Bank Card T&C</Typography>
+                <Typography><Badge/>Buy this product and Get Extra 10% Up to ₹500 off on Home & Kitchen Appliances Know More</Typography>
+                <Typography><Badge/>Purchase this product & win a surprise cashback coupon for The Big Billion Days Sale 2022 Know More</Typography>
+                <Typography><Badge/>Sign up for Flipkart Pay Later and get Flipkart Gift Card worth upto ₹500* Know More</Typography>
+                <Typography><Badge/>No Cost EMI on Bajaj Finsery EMI cart value abouve ₹2999  T&C</Typography>
+            </Box>  
+        </>
     );
 };
 
