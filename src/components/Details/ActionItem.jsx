44e327e68a669bcfc5011e39dbbc9d23c81bdfe3
khanshorif331 <khanshorif331@gmail.com>
2022-08-27 13:10:27 +0600

    image styling done for single product

--- src/components/Details/ActionItem.jsx
+++ src/components/Details/ActionItem.jsx
@@ -5,15 +5,19 @@ const LeftContainer = styled(Box)`
     min-width: 40%;
     padding : 40px 0 0 80px;
 `
+const Image = styled('img')({
+    padding : '15px 20px',
+    border : '1px solid #f0f0f0'
+})
 
 
 const ActionItem = ({product}) => {
     return (
-        <Box>
-            <img src={product.detailUrl} alt="" />
+        <LeftContainer >
+            <Image src={product.detailUrl} alt="product" />
             <Button variant='contained'>Add to Cart</Button>
             <Button variant='contained'>Buy Now</Button>
-        </Box>
+        </LeftContainer>
     );
 };
 
