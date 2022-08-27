9ee89f5d0966b8d83cb0a10077d0cea2fffd5991
khanshorif331 <khanshorif331@gmail.com>
2022-08-27 13:47:48 +0600

    fixed some bugs of the product details page image

--- src/components/Details/DetailView.jsx
+++ src/components/Details/DetailView.jsx
@@ -44,8 +44,13 @@ const DetailView = () => {
                         <Typography style={{marginTop : 5,color:'#878787',fontSize:14}}>
                             8 Ratings & 1 Reviews
                             <Box component='span'>
-                                <img src={fassured} alt="" />
+                                <img src={fassured} style={{width:77,marginLeft:20}} alt="" />
                             </Box>
+                        </Typography>
+                        <Typography>
+                            <Box component='span' style={{fontSize:28}}>₹{product.price.cost}</Box>&nbsp;&nbsp;&nbsp; 
+                            <Box component='span' style={{color:'#878787'}}><strike>{product.price.mrp}</strike></Box>&nbsp;&nbsp;&nbsp;
+                            <Box component='span' style={{color:'#388E3C'}}>{product.price.discount}</Box>
                         </Typography>
                     </RightContainer>
                 </Container>
