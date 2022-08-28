a085ab5bc0911f02933cf27344893201d129b36f
khanshorif331 <khanshorif331@gmail.com>
2022-08-28 20:48:22 +0600

    search functionality  implemented

--- src/components/Header/Search.jsx
+++ src/components/Header/Search.jsx
@@ -1,7 +1,11 @@
-import { Box, InputBase,styled } from '@mui/material';
+import { Box, InputBase,List,ListItem,styled } from '@mui/material';
 import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
+import { useState } from 'react';
 
+import {useSelector,useDispatch} from 'react-redux'
+import {getProducts} from '../../redux/actions/productActions'
+import { useEffect } from 'react';
 
 const SearchContainer = styled(Box)`
     background-color: #fff;
@@ -25,15 +29,39 @@ const SearchIconWrapper = styled(Box)`
 
 
 const Search = () => {
+    const [text,setText] = useState('')
+    const dispatch = useDispatch()
+    const {products} = useSelector(state => state.getProducts)
+
+    useEffect( () => {
+        dispatch(getProducts())
+    },[dispatch])
+
+    const getText = (text) => {
+        setText(text)
+    }
     return (
         <SearchContainer>
             <InputSearchBase
             placeholder='Search for products, brands and more'
+            onChange={(e) => getText(e.target.value)}
             >
             </InputSearchBase>
             <SearchIconWrapper>
                 <SearchIcon/>
             </SearchIconWrapper>
+            {
+                text && 
+                <List>
+                    {
+                        products.filter(product => product.title.longTitle.toLowerCase().includes(text.toLowerCase())).map(product => {
+                            return <ListItem key={product.id}>
+                                {product.title.longTitle}
+                            </ListItem>
+                        })
+                    }
+                </List>
+            }
         </SearchContainer>
     );
 };
