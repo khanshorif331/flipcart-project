270859f807cd3db5007701193af5cbc1594e638a
khanshorif331 <khanshorif331@gmail.com>
2022-08-28 22:38:11 +0600

    remoove from cart action created

--- src/redux/actions/cartAction.js
+++ src/redux/actions/cartAction.js
@@ -3,7 +3,7 @@ import * as actionTypes from '../constants/cartConstant'
 
 const URL = 'http://localhost:5000'
 
-// using thunk as middleware for async action
+// using thunk as middleware for async action and dispatch is coming from this
 export const addToCart = (id, quantity) => async dispatch => {
 	try {
 		const { data } = await axios.get(`${URL}/product/${id}`)
@@ -17,4 +17,13 @@ export const addToCart = (id, quantity) => async dispatch => {
 	}
 }
 
-export const removeFromCart = () => {}
+// using thunk as middleware and dispatch is coming from this
+export const removeFromCart = id => dispatch => {
+	dispatch({ type: actionTypes.REMOVE_FROM_CART, payload: id })
+	// try{
+
+	// }
+	// catch(err){
+	//     dispatch({ type: actionTypes.ADD_TO_CART_ERROR, payload: err.message })
+	// }
+}
