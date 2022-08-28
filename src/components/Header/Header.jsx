3809619f41f52f63bad576387707a1162b0fd89a
khanshorif331 <khanshorif331@gmail.com>
2022-08-28 19:55:54 +0600

    drawer added in the menu button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
-import { AppBar,Toolbar , styled, Box, Typography, IconButton} from '@mui/material';
+import { AppBar,Toolbar , styled, Box, Typography, IconButton,Drawer} from '@mui/material';
 import React from 'react';
 import CustomButtons from './CustomButtons';
 import Search from './Search';
 import { Link } from 'react-router-dom';
 import {Menu} from '@mui/icons-material';
+import { useState } from 'react';
 
 const StyledHeader = styled(AppBar)`
   background-color: #2874f0;
@@ -47,13 +48,27 @@ const MenuButton = styled(IconButton)(({theme}) => ({
 const Header = () => {
   const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
   const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
+  const [open,setOpen] = useState(false);
+
+  const handleOpen = () => {
+      setOpen(true)
+  }
+  const handleClose = () => {
+      setOpen(false)
+  }
 
     return (
            <StyledHeader>
             <Toolbar style={{minHeight:55}}>
-              <MenuButton>
+              <MenuButton color='inherit' onClick={handleOpen}>
                 <Menu />
               </MenuButton>
+              <Drawer
+                open={open}
+                onClose={handleClose}
+              >
+                Hello
+              </Drawer>
               <Component to='/'>
                 <img src={logoURL} alt="logo" style={{width:75}}/>
                 <Box style={{display:'flex'}}>
