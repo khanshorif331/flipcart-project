4dcc7ef3651daf4716d6c892f1e9e71ccfd16d31
khanshorif331 <khanshorif331@gmail.com>
2022-08-29 19:19:10 +0600

    single cart item compoonent added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,17 +1,23 @@
 import { Grid, Typography ,Box} from '@mui/material';
 import React from 'react';
 import {useSelector} from 'react-redux'
+import CartItem from './CartItem';
 const Cart = () => {
     const {cartItems} = useSelector(state => state.cart)
     return (
         <>
            {
-                cartItems ? 
+                cartItems.length ? 
                 <Grid container>
                     <Grid item lg={9} md={9} sm={12} xs={12}>
                         <Box>
                             <Typography>My Cart ({cartItems.length})</Typography>
                         </Box>
+                        {
+                            cartItems.map(item => (
+                                <CartItem></CartItem>
+                            ))
+                        }
                     </Grid>
                     <Grid item lg={3} md={3} sm={12} xs={12}></Grid>
                 </Grid>
