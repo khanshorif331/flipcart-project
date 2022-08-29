9ea991dfc5a8aa73bb016bc0ba49cdb772d37678
khanshorif331 <khanshorif331@gmail.com>
2022-08-29 19:30:07 +0600

    single cart data passed to the component throough props

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,30 +1,41 @@
-import { Grid, Typography ,Box} from '@mui/material';
+import { Grid, Typography ,Box,styled} from '@mui/material';
 import React from 'react';
 import {useSelector} from 'react-redux'
 // compoonent
 import CartItem from './CartItem';
 import TotalView from './TotalView';
+
+
+const Container = styled(Grid) `
+    padding : 30px 135px; 
+`
+
+const Header = styled(Box)`
+    padding : 15px 24px
+`
+
+
 const Cart = () => {
     const {cartItems} = useSelector(state => state.cart)
     return (
         <>
            {
                 cartItems.length ? 
-                <Grid container>
+                <Container container>
                     <Grid item lg={9} md={9} sm={12} xs={12}>
-                        <Box>
+                        <Header>
                             <Typography>My Cart ({cartItems.length})</Typography>
-                        </Box>
+                        </Header>
                         {
                             cartItems.map(item => (
-                                <CartItem></CartItem>
+                                <CartItem item={item}></CartItem>
                             ))
                         }
                     </Grid>
                     <Grid item lg={3} md={3} sm={12} xs={12}>
                         <TotalView></TotalView>
                     </Grid>
-                </Grid>
+                </Container>
                 :
                 <div>Empty</div>
            }
