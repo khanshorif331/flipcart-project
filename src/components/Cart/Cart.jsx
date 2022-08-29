4dfd7d614a48ad1f1ba95ea99f3090ef060113b4
khanshorif331 <khanshorif331@gmail.com>
2022-08-30 02:46:03 +0600

    place order button styling

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -15,6 +15,13 @@ const Header = styled(Box)`
     background : '#fff'
 `
 
+const WrapperButton = styled(Box)`
+    padding : 16px 22px ;
+    background : #fff ;
+    box-shadow : 0 2px 10px 0 rgb(0 0 0 / 10%);
+    border-top : 1px solid #f0f0f0
+`
+
 
 const Cart = () => {
     const {cartItems} = useSelector(state => state.cart)
@@ -32,9 +39,9 @@ const Cart = () => {
                                 <CartItem item={item}></CartItem>
                             ))
                         }
-                        <Box>
+                        <WrapperButton>
                             <Button>Place Order</Button>
-                        </Box>
+                        </WrapperButton>
                     </Grid>
                     <Grid item lg={3} md={3} sm={12} xs={12}>
                         <TotalView></TotalView>
