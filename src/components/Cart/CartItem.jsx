25dabe96d3401c33ae9a0114b1f020819d1b609c
khanshorif331 <khanshorif331@gmail.com>
2022-08-29 20:04:52 +0600

    some styled added in the producdetai cooomponent

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,10 +1,30 @@
+import { Box, Typography,styled } from '@mui/material';
 import React from 'react';
 
-const CartItem = () => {
+
+const Compoonent = styled(Box)`
+    border-top : 1px solid #f0f0f0 ;
+    display : flex
+`
+
+const LeftComponent = styled(Box)`
+    margin : 20px
+`
+
+const CartItem = ({item}) => {
+    const fassured = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png'
     return (
-        <div>
-            Hello
-        </div>
+        <Compoonent>
+            <LeftComponent>
+                <img src={item.url} alt="product" />
+            </LeftComponent>
+            <Box>
+                <Typography>{item.title.longTitle}</Typography>
+                <Typography>Seller : RetailNet
+                    <Box component='span'><img src={fassured} style={{width:50,marginLeft:10}} alt="Flipcart asssured" /></Box>
+                </Typography>
+            </Box>
+        </Compoonent>
     );
 };
 
