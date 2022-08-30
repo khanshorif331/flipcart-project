b6550e366ea4e2d06584bfe63b3ce81abd475cd7
khanshorif331 <khanshorif331@gmail.com>
2022-08-31 01:29:03 +0600

    fixed some responsive issue in the cart cooomponent

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,13 @@ import TotalView from './TotalView';
 import EmptyCart from './EmptyCart';
 
 
-const Container = styled(Grid) `
-    padding : 30px 135px; 
-`
+const Container = styled(Grid) (({theme})=>({
+    padding : '30px 135px',
+    [theme.breakpoints.down('sm')]:{
+        padding : '15px  0'
+    } 
+}))
+
 
 const Header = styled(Box)`
     padding : 15px 24px ; 
@@ -31,6 +35,14 @@ const StyledButton = styled(Button)`
     width : 220px;
 `
 
+const LeftComponent = styled(Grid) (({theme})=>({
+    paddingRight : '15px',
+    [theme.breakpoints.down('sm')]:{
+        marginBottom : '15px'
+    }
+}))
+
+
 
 const Cart = () => {
     const {cartItems} = useSelector(state => state.cart)
@@ -39,7 +51,7 @@ const Cart = () => {
            {
                 cartItems.length ? 
                 <Container container>
-                    <Grid item lg={9} md={9} sm={12} xs={12}>
+                    <LeftComponent item lg={9} md={9} sm={12} xs={12}>
                         <Header>
                             <Typography>My Cart ({cartItems.length})</Typography>
                         </Header>
@@ -51,7 +63,7 @@ const Cart = () => {
                         <WrapperButton>
                             <StyledButton>Place Order</StyledButton>
                         </WrapperButton>
-                    </Grid>
+                    </LeftComponent>
                     <Grid item lg={3} md={3} sm={12} xs={12}>
                         <TotalView cartItems={cartItems}></TotalView>
                     </Grid>
